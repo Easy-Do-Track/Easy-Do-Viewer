00fdf43a0ac90b7cca5ee32234abbf2f70a0f337
dfkdream <dfkdream@gmail.com>
2023-05-16 21:59:51 +0900

    Add websocket

--- src/components/preview.jsx
+++ src/components/preview.jsx
@@ -1,5 +1,32 @@
+import { useState, useEffect } from "react"
+
 export function Preview(){
+    const [data, setData] = useState(0);
+    const [lastUpdate, setLastUpdate] = useState(0);
+
+    useEffect(()=>{
+        // 여기서 webSocket 연결
+        let ws = new WebSocket("ws://localhost:8080/stream");
+
+        ws.onmessage = (evt)=>{
+            setData(evt.data);
+            setLastUpdate(new Date())
+        }
+
+        return ()=>{
+            // 여기서 cleanup
+            if (!ws){
+                return;
+            }
+
+            ws.close();
+        };
+    }, []);
+
     return <div className="preview">
-        <p>여기에 트래킹 로그 출력</p>
+        last update: {lastUpdate.toString()}
+        <pre>
+            {data}
+        </pre>
     </div>
 }
